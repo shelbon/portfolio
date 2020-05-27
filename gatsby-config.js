962771eb09d7b20e207609cf08e16eecac311978
sheron <mouclesheron@gmail.com>
2020-05-27 12:58:19 -0400

    init repository

---
+++ gatsby-config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  siteMetadata: {
+    title: `Patrick Shéron Moucle Portfolio`,
+    description: `Patrick shéron moucle  développeur web et mobile,voici mon portfolio.`,
+    author: `@PSMoucle`,
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // `gatsby-plugin-offline`,
+  ],
+}

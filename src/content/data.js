82e89d893d1faa8b2b43a6c4843adc28acd01a6a
sheron <mouclesheron@gmail.com>
2020-06-25 15:08:43 -0400

    add dummy projects data

---
+++ src/content/data.js
@@ -0,0 +1,23 @@
+import {v4 as uuidv4} from "uuid";
+export const projectsData = [
+    {
+      id: uuidv4(),
+      img: 'placeholder.jpg',
+      title: 'placeholder',
+      info: '',
+      info2: '',
+      url: '',
+      repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    },
+    {
+      id: uuidv4(),
+      img: 'placeholder2.jpg',
+      title: '',
+      info: '',
+      info2: '',
+      url: '',
+      repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    }
+     
+  ];
+  

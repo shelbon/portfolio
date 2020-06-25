f537eb717c90e718ae10b9de367fa410909c96f5
sheron <mouclesheron@gmail.com>
2020-06-25 15:01:37 -0400

    change  hamburger nav bar to bottom navbar

--- src/components/nav.js
+++ src/components/nav.js
@@ -2,35 +2,9 @@ import { Link } from "gatsby"
 import PropTypes, { bool } from "prop-types"
 import React from "react"
 import navStyles from "./nav.module.css"
-import { FaBars } from "react-icons/fa"
+import { FaHome,FaBriefcase,FaAddressCard } from "react-icons/fa"
 export default class Nav extends React.Component {
-  componentDidMount() {
-    this.init()
-  }
-  init() {
-    const menu = document.querySelector(".js-menu");
-    const menuButton = document.getElementById("js-nav-toggle");
-    const navLinks=document.querySelectorAll(`.${navStyles.nav__link}`);
-    console.log(navLinks);
-    menuButton.addEventListener(
-      "click",
-      function () {
-        menu.classList.toggle(navStyles.navOpen);
-        if (menu.classList.contains(navStyles.navOpen)) {
-          menu.setAttribute("aria-hidden", "false")
-          menu.setAttribute("aria-expanded", "true")
-        } else {
-          menu.setAttribute("aria-hidden", "true")
-          menu.setAttribute("aria-expanded", "false")
-        }
-      }
-    );
-    navLinks.forEach(link => {
-      link.addEventListener('click', () => {
-          menu.classList.toggle(navStyles.navOpen);
-      });
-    });
-  }
+  
   render() {
     return (
       <nav
@@ -39,39 +13,28 @@ export default class Nav extends React.Component {
         aria-hidden="false"
         className="js-menu"
       >
-        <div className={navStyles.nav__header}>
-          <Link className={navStyles.nav__brand} to="/">
-            Moucle patricksheron
-          </Link>
-          <button
-            className={navStyles.navToggle}
-            id="js-nav-toggle"
-            type="button"
-            aria-label="Menu"
-            aria-controls="nav"
-          >
-            <FaBars className={navStyles.nav__hamburger} />
-          </button>
-        </div>
         <ul className={navStyles.nav__list}>
           <li className={navStyles.nav__item}>
             <Link   className={navStyles.nav__link} to="/#home" >
-              Accueil
+              <FaHome className={navStyles.nav__link__icon}/>
+              <p>Accueil</p>
               </Link>
           </li>
           <li className={navStyles.nav__item}>
             <Link className={navStyles.nav__link} to="/#about" >
-              About
+              <p>About</p>
             </Link>
           </li>
           <li className={navStyles.nav__item}>
             <Link className={navStyles.nav__link} to="/#project">
-              Réalisation
+            <FaBriefcase className={navStyles.nav__link__icon} />
+             <p>Réalisation</p>
             </Link>
           </li>
           <li className={navStyles.nav__item}>
             <Link className={navStyles.nav__link} to="/#contact" >
-              Contact
+              <FaAddressCard className={navStyles.nav__link__icon}/>
+              <p>Contact</p>
             </Link>
           </li>
         </ul>

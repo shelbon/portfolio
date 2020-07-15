16614e7d94e285d574e12fdaec3fa50b6506c0f8
Sheron moucle <mouclesheron@gmail.com>
2020-07-15 10:46:03 -0400

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,34 +1,50 @@
 import React from "react"
 
 import Layout from "../components/layout"
-import IndexStyles from "../components/index.module.css"
+import IndexStyles from "../styles/index.module.css"
 import SEO from "../components/seo"
-import ProjectGallery from "../components/project-gallery.js"
+import ProjectGallery from "../components/project-gallery"
 import { Link } from "gatsby"
-const IndexPage = () => (
+import ContactForm  from "../components/contact"
+
+
+const IndexPage = ( ) => (
   <Layout>
     <SEO title="Home" />
     <div>
+       
       <section
         id="home"
         className={`${IndexStyles.section}
-                                     ${IndexStyles.sectionIntro}
-                                    ${IndexStyles.section__bgSilverSand}`}
+                                     ${IndexStyles.sectionIntro} ${IndexStyles.section__bgSilverSand}`}
       >
         <h1
           className={`${IndexStyles.section__title} ${IndexStyles.section__titleIntro} ${IndexStyles.section__titleBlack}`}
         >
-          <p>Je suis<strong>Patrick shéron moucle
-             Développeur informatique</strong></p>
+          <p>
+            <strong>je créer les meilleur  site web et application mobile</strong>
+            Je suis Patrick shéron moucle
+             Développeur informatique</p>
            
         </h1>
-        <Link to="/#project" className={IndexStyles.section_intro__link}><p>mes réalisations</p></Link>
+        <Link to="/#project" className={IndexStyles.section__intro__link}><p>mes réalisations</p></Link>
       </section>
-
+      <section
+        id="project"
+        className={`${IndexStyles.section} ${IndexStyles.sectionProject}`}
+      >
+        <div className={IndexStyles.section__container}>
+        <h2 className={IndexStyles.section__titleProject}>Réalisations</h2>
+        <p className={IndexStyles.section__subtitleProject}>
+        Voici des exemples de mes réalisations personnelles et professionnelles.
+        </p>
+         
+        <ProjectGallery className={IndexStyles.sectionProject} />
+        </div>
+      </section>
       <section
         id="about"
-        className={`${IndexStyles.section}
-                                    ${IndexStyles.section__bgOxfordBlueAbout}
+        className={`${IndexStyles.section}             
                                     ${IndexStyles.sectionAbout}`}
       >
         <div className={IndexStyles.section__container}>
@@ -39,23 +55,10 @@ const IndexPage = () => (
           </p>
         </div>
       </section>
-      <section
-        id="project"
-        className={`${IndexStyles.section__bgOxfordBlueProject} ${IndexStyles.section} ${IndexStyles.sectionProject}`}
-      >
-        <div className={IndexStyles.section__container}>
-        <h2 className={IndexStyles.section__titleProject}>My work</h2>
-        <p className={IndexStyles.section__subtitleProject}>
-          A selection of my range of work
-        </p>
-        <ProjectGallery />
-        </div>
-      </section>
-      <section id="contact" className={IndexStyles.section__bgSilverSand}>
-        <p>about</p>
-      </section>
+     
+      <ContactForm id="contact"/>
     </div>
- 
+  <script src="./test.js" />
   </Layout>
 )
 export default IndexPage

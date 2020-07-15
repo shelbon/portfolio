367ab809327a6141bed773c088234d2936200929
Sheron moucle <mouclesheron@gmail.com>
2020-07-15 10:44:00 -0400

    add  possibility to view  resume  file

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,11 +1,19 @@
-import { Link } from "gatsby"
+import { Link,useStaticQuery } from "gatsby"
 import PropTypes, { bool } from "prop-types"
 import React from "react"
-import navStyles from "./nav.module.css"
-import { FaHome,FaBriefcase,FaAddressCard } from "react-icons/fa"
-export default class Nav extends React.Component {
-  
-  render() {
+import navStyles from "../styles/nav.module.css"
+import { FaHome, FaBriefcase, FaAddressCard } from "react-icons/fa"
+
+
+ const  Nav = ()=> {
+  let cv = useStaticQuery(graphql`
+  {
+    file(name: {regex: "/\\\\CV/"}) {
+    publicURL
+    }
+  }
+`);
+ console.log(cv);
     return (
       <nav
         id={navStyles.nav}
@@ -15,30 +23,35 @@ export default class Nav extends React.Component {
       >
         <ul className={navStyles.nav__list}>
           <li className={navStyles.nav__item}>
-            <Link   className={navStyles.nav__link} to="/#home" >
-              <FaHome className={navStyles.nav__link__icon}/>
+            <Link className={navStyles.nav__link} to="/#home">
+              <FaHome className={navStyles.nav__link__icon} />
               <p>Accueil</p>
-              </Link>
+            </Link>
           </li>
           <li className={navStyles.nav__item}>
-            <Link className={navStyles.nav__link} to="/#about" >
+            <Link className={navStyles.nav__link} to="/#about">
               <p>About</p>
             </Link>
           </li>
           <li className={navStyles.nav__item}>
             <Link className={navStyles.nav__link} to="/#project">
-            <FaBriefcase className={navStyles.nav__link__icon} />
-             <p>Réalisation</p>
+              <FaBriefcase className={navStyles.nav__link__icon} />
+              <p>Réalisation</p>
             </Link>
           </li>
           <li className={navStyles.nav__item}>
-            <Link className={navStyles.nav__link} to="/#contact" >
-              <FaAddressCard className={navStyles.nav__link__icon}/>
+            <Link className={navStyles.nav__link} to="/#contact">
+              <FaAddressCard className={navStyles.nav__link__icon} />
               <p>Contact</p>
             </Link>
+          </li>
+          <li className={navStyles.nav__item}>
+            <a className={navStyles.nav__link} href={cv.file.publicURL}>
+              <p>CV</p>
+            </a>
           </li>
         </ul>
       </nav>
     )
-  }
-}
+    }
+    export default Nav;

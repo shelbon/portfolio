265cde29ebb1c10cb142493783aa9fd031f94d1e
Sheron moucle <mouclesheron@gmail.com>
2020-07-15 10:37:36 -0400

    Update gatsby-config.js
    add  portfolio-theme-core
    add postcss with autoprefixer

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,4 @@
+
 module.exports = {
   siteMetadata: {
     title: `Patrick Shéron Moucle Portfolio`,
@@ -6,6 +7,12 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
+    {
+      resolve: `@arshad/gatsby-theme-portfolio-core`,
+      options: {
+        basePath: `/projects`,
+      }
+    },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -16,11 +23,35 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
+        name: `content`,
+        path: `${__dirname}/content/`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
         name: `pages`,
         path: `${__dirname}/src/pages/`,
       },
     },
     `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
+    `gatsby-plugin-sharp`,{
+    resolve: `gatsby-plugin-postcss`,
+    options: {
+      postCssPlugins: [
+        require('postcss-preset-env')({
+           autoprefixer: { grid: true },
+           features: {
+               'nesting-rules': true
+           },
+           browsers: [
+               '> 1%',
+               'last 2 versions',
+               'Firefox ESR',
+           ]
+       })
+    ]
+    }
+    }
   ],
 }

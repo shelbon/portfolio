0c67ae7627e4c21dc5bac7b34f7c502c1cab46a3
Sheron moucle <mouclesheron@gmail.com>
2020-07-27 17:04:49 -0400

    modify validation form,delegate to yup

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,77 +1,136 @@
-import React from 'react';
-import { Formik, Form, Field, ErrorMessage } from 'formik';
-import ContactFormStyles from "../styles/contactform.module.css";
-import useEmailAutocomplete from 'use-email-autocomplete';
- 
+import React from "react"
+import * as yup from 'yup';
+import { Formik, Form, Field, ErrorMessage ,withFormik} from "formik"
+import ContactFormStyles from "../styles/contactform.module.css"
+import useEmailAutocomplete from "use-email-autocomplete"
+
+let schema = yup.object().shape({
+  name:yup.string().min(5,"too short").required("Required"),
+  email: yup.string().email("Valid Email Required").required("Required"),
+  message:yup.string().required("Required")
   
-export default function ContactForm() {
-  const { email, bind } = useEmailAutocomplete();
+});
+ export default function ContactForm() {
+  const { email, bind } = useEmailAutocomplete()
   
-  return(
-          
+  return (
     <div className={ContactFormStyles.container}>
-    <h1>Ecrivez-Moi</h1>
-    <Formik
-      initialValues={{  "bot-field": "",
-      "form-name": "contact",name: '', email: '',message:'' }}
-      validate={values => {
-        const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
-        const errors = {};
-        if(!values.name){
-            errors.name = 'Required';
-        }
-        if(!values.message){
-            errors.message = 'Required';
-        }
-        if(!values.email || !emailRegex.test(values.email)) 
-        {        errors.email = 'Valid Email Required'      
-         }
-        if (!values.email) {
-          errors.email = 'Required';
-        }  
+      <h1>Ecrivez-Moi</h1>
+      <Formik
+        initialValues={{
+          "bot-field": "",
+          "form-name": "contact",
+          name: "",
+          email: "",
+          message: "",
+        }}
          
-        return errors;
-      }}
-      onSubmit={(values, { setSubmitting }) => {
-        const descriptor = {
-          from: `${values.email}`,
-          to: "mouclesheron@gmail.com",
-          subject: `${values.name} sent you a message `,
-          text: values.message,
-        }
-         
-      }}
-    >
-      {({
-        values,
-        errors,
-        touched,
-        handleChange,
-        handleBlur,
-        handleSubmit,
-        isSubmitting,
-        /* and other goodies */
-      }) => (
-        <Form name="contact" method="post"   className={ContactFormStyles.form}action="">
+        validationSchema={schema}
 
-        <label htmlFor="name" className={ContactFormStyles.form__label__name}>Name: </label>
-        <Field type="text" name="name"  className={ContactFormStyles.form__input__name} />
-        <ErrorMessage name="name" component="div" className={ContactFormStyles.form__error__name} />
+        onSubmit={(values, { setSubmitting }) => {
+          const descriptor = {
+            from: `${values.email}`,
+            to: "mouclesheron@gmail.com",
+            subject: `${values.name} sent you a message `,
+            text: values.message,
+          }
+        }}
+      >
+        {({
+          values,
+          errors,
+          touched,
+          handleChange,
+          handleBlur,
+          handleSubmit,
+          isSubmitting,
+          /* and other goodies */
+        }) => (
+          <Form
+            name="contact"
+            method="post"
+            className={ContactFormStyles.form}
+            action=""
+          >
+            <label
+              htmlFor="name"
+              className={ContactFormStyles.form__label__name}
+            >
+              Name:{" "}
+            </label>
+            <Field
+              type="text"
+              name="name"
+              className={ContactFormStyles.form__input__name}
+            />
+            <ErrorMessage
+              name="name"
+              component="div"
+              className={ContactFormStyles.form__error__name}
+            />
 
-        <label htmlFor="email" className={ContactFormStyles.form__label__email}>Email: </label>
-        <Field type="email" name="email" {...bind} className={ContactFormStyles.form__input__email} />
-        <ErrorMessage name="email"   component="div" className={ContactFormStyles.form__error__email} />
-        
-        <label htmlFor="message"  className={ContactFormStyles.form__label__message} >Message: </label>
-        <Field type="text" name="message"  component="textarea"     className={ContactFormStyles.form__input__message} />
-        <ErrorMessage name="message"   component="div"  className={ContactFormStyles.form__error__message}/>
-        
-        <button type="submit" disabled={isSubmitting} className={ContactFormStyles.form__input__submit}>
-          Submit
-        </button>
-      </Form>
-      )}
-    </Formik>
-  </div>)
+            <label
+              htmlFor="email"
+              className={ContactFormStyles.form__label__email}
+            >
+              Email:{" "}
+            </label>
+            <Field
+              name="email"
+              
+              placeholder="email here"
+            >
+              {({
+                field, // { name, value, onChange, onBlur }
+
+                form: { touched, errors }, // also values, setXXXX, handleXXXX, dirty, isValid, status, etc.
+
+                meta,
+              }) => (
+                <div>
+                  <input type="email" placeholder="Email" {...field} {...bind} className={ContactFormStyles.form__input__email} />
+
+                  {meta.touched && meta.error && (
+                    <div className="error">{meta.error}</div>
+                  )}
+                </div>
+              )}
+            </Field>
+            <ErrorMessage
+              name="email"
+              component="div"
+              className={ContactFormStyles.form__error__email}
+            />
+
+            <label
+              htmlFor="message"
+              className={ContactFormStyles.form__label__message}
+            >
+              Message:{" "}
+            </label>
+            <Field
+              type="text"
+              name="message"
+              component="textarea"
+              className={ContactFormStyles.form__input__message}
+            />
+            <ErrorMessage
+              name="message"
+              component="div"
+              className={ContactFormStyles.form__error__message}
+            />
+
+            <button
+              type="submit"
+              disabled={isSubmitting}
+              className={ContactFormStyles.form__input__submit}
+            >
+              Submit
+            </button>
+          </Form>
+        )}
+      </Formik>
+    </div>
+  )
 }
  

b413c9d9b1e0efde74ccb165805ea169dd0926dd
Sheron moucle <mouclesheron@gmail.com>
2020-08-03 19:44:45 -0400

    add recaptcha to the  contact form

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,21 +1,18 @@
 import React from "react"
 import * as yup from 'yup';
-import { Formik, Form, Field, ErrorMessage ,withFormik} from "formik"
 import ContactFormStyles from "../styles/contactform.module.css"
-import useEmailAutocomplete from "use-email-autocomplete"
-
-let schema = yup.object().shape({
-  name:yup.string().min(5,"too short").required("Required"),
-  email: yup.string().email("Valid Email Required").required("Required"),
-  message:yup.string().required("Required")
-  
-});
+import MyEmail from '../templates/email'
+import Recaptcha from 'react-google-recaptcha';
+import { Formik, Form, Field, ErrorMessage } from "formik"
+import { renderEmail } from 'react-html-email'
+ 
+ 
+ 
  export default function ContactForm() {
-  const { email, bind } = useEmailAutocomplete()
+ 
   
   return (
     <div className={ContactFormStyles.container}>
-      <h1>Ecrivez-Moi</h1>
       <Formik
         initialValues={{
           "bot-field": "",
@@ -23,17 +20,31 @@ let schema = yup.object().shape({
           name: "",
           email: "",
           message: "",
+          recaptcha: '',
+          success: false,
         }}
          
-        validationSchema={schema}
+        validationSchema={yup.object().shape({
+          name:yup.string().min(5,"Too short").required("Required"),
+          email: yup.string().email("Valid Email Required").required("Required"),
+          message:yup.string().required("Required"),
+          recaptcha: yup.string().required('Robots are not welcome yet!'),
+          
+        })}
 
-        onSubmit={(values, { setSubmitting }) => {
+        onSubmit={async (values, { setSubmitting }) => {
+          const messageHtml =  renderEmail(
+            <MyEmail name={this.state.name}> {this.state.feedback}</MyEmail>
+          );
+          
           const descriptor = {
             from: `${values.email}`,
             to: "mouclesheron@gmail.com",
             subject: `${values.name} sent you a message `,
             text: values.message,
+            html:messageHtml,
           }
+           
         }}
       >
         {({
@@ -44,6 +55,7 @@ let schema = yup.object().shape({
           handleBlur,
           handleSubmit,
           isSubmitting,
+          setFieldValue
           /* and other goodies */
         }) => (
           <Form
@@ -54,76 +66,88 @@ let schema = yup.object().shape({
           >
             <label
               htmlFor="name"
-              className={ContactFormStyles.form__label__name}
+              className={ContactFormStyles.form__label}
             >
-              Name:{" "}
+              Name: 
             </label>
             <Field
               type="text"
               name="name"
-              className={ContactFormStyles.form__input__name}
+              className={ContactFormStyles.form__input}
+              required
+              placeholder="Name"
+              aria-invalid={errors.name  && touched.name ?"true":"false"}
             />
             <ErrorMessage
               name="name"
               component="div"
-              className={ContactFormStyles.form__error__name}
+              className={ContactFormStyles.form__error}
             />
 
             <label
               htmlFor="email"
-              className={ContactFormStyles.form__label__email}
+              className={ContactFormStyles.form__label}
             >
-              Email:{" "}
+              Email: 
             </label>
             <Field
               name="email"
-              
-              placeholder="email here"
-            >
-              {({
-                field, // { name, value, onChange, onBlur }
-
-                form: { touched, errors }, // also values, setXXXX, handleXXXX, dirty, isValid, status, etc.
-
-                meta,
-              }) => (
-                <div>
-                  <input type="email" placeholder="Email" {...field} {...bind} className={ContactFormStyles.form__input__email} />
-
-                  {meta.touched && meta.error && (
-                    <div className="error">{meta.error}</div>
-                  )}
-                </div>
-              )}
-            </Field>
+              placeholder="Email"  
+              id="email"
+              value={values.email}
+              className={ContactFormStyles.form__input}
+              required
+              aria-invalid={errors.email  && touched.email ?"true":"false"}
+            />
             <ErrorMessage
               name="email"
               component="div"
-              className={ContactFormStyles.form__error__email}
+              className={ContactFormStyles.form__error}
             />
 
             <label
               htmlFor="message"
-              className={ContactFormStyles.form__label__message}
+              className={ContactFormStyles.form__label}
             >
-              Message:{" "}
+              Message:
             </label>
             <Field
               type="text"
               name="message"
+              placeholder="Message"
               component="textarea"
-              className={ContactFormStyles.form__input__message}
+              className={` ${ContactFormStyles.form__inputMessage}
+                          ${ContactFormStyles.form__input}`}
+              required
+              aria-invalid={errors.message  && touched.message ?"true":"false"}
             />
             <ErrorMessage
               name="message"
               component="div"
-              className={ContactFormStyles.form__error__message}
+              className={ContactFormStyles.form__error}
             />
-
+            {console.log({email:values.email})}
+             {values.name && values.email && values.message && (
+          <div className={ContactFormStyles.form__recaptcha__container}>
+            <Field
+              component={Recaptcha}
+              sitekey={process.env.GATSBY_PORTFOLIO_RECAPTCHA_KEY}
+              name="recaptcha"
+               
+              onChange={value => setFieldValue('recaptcha', value)}
+            />
+            <ErrorMessage component="span" style={{color: "#ff4136",}} name="recaptcha" />
+          </div>
+        )}
+        {values.success && (
+          <div className={ContactFormStyles.form__success}>
+              <h4>Your message has been successfully sent, I will get back to you ASAP!</h4>
+          </div>
+        )}
             <button
               type="submit"
               disabled={isSubmitting}
-              className={ContactFormStyles.form__input__submit}
+              className={ContactFormStyles.form__inputSubmit}
             >
               Submit
             </button>

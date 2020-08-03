dc070d0f3e5ab63977da8b68a1efc182f32acc76
Sheron moucle <mouclesheron@gmail.com>
2020-08-03 19:48:06 -0400

    fix:layout behavior on resize

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,30 +9,29 @@ import ContactForm  from "../components/contact"
 import DevIllustration from "../components/illustration/DevIllustration/index"
 import Header from "../components/header"
 import Footer from "../components/footer"
-
-
+ 
+import aboutme from "../images/about_me.svg"
 
 const IndexPage = ( ) => (
   <>
-  <div className={`${IndexStyles.wrapper}`}>
+  <div className={IndexStyles.wrapper}>
     <SEO title="Home" />
        <Header/>
       <section
         id="home"
-        className={`${IndexStyles.section}
-                                     ${IndexStyles.sectionIntro}   `}
+        className={IndexStyles.sectionIntro}
       >
         <div className={IndexStyles.section__intro__details__container}>
         <h1
           className={`${IndexStyles.section__title} ${IndexStyles.section__titleIntro} ${IndexStyles.section__titleBlack}`}
         >
-          <p>
             <strong>je créer les meilleur  site web et application mobile</strong>
             <br/> Je suis Patrick shéron moucle
-             Développeur informatique</p>
+             Développeur informatique
+             
            
         </h1>
-        <Link to="/#project" className={IndexStyles.section__intro__link}><p>mes réalisations</p></Link>
+        <Link to="/#project" className={IndexStyles.section__intro__link}><p>Mes réalisations</p></Link>
         </div>
         <div className={IndexStyles.section__intro__illustration__container}>
         <DevIllustration/>
@@ -40,6 +39,7 @@ const IndexPage = ( ) => (
         
       </section>
       </div>
+       
       <section
         id="project"
         className={`${IndexStyles.section} ${IndexStyles.sectionProject} `}
@@ -53,21 +53,32 @@ const IndexPage = ( ) => (
         <ProjectGallery className={IndexStyles.sectionProject} />
         </div>
       </section>
+      <div className={IndexStyles.wrapperAbout}>
       <section
         id="about"
         className={`${IndexStyles.section}             
                                     ${IndexStyles.sectionAbout}`}
       >
-        <div className={IndexStyles.section__container}>
-          <h2 className={` ${IndexStyles.section__about__title} ${IndexStyles.section__titleWhite} `}>&Agrave; propos</h2>
-          <p className={IndexStyles.section__about__body}>
-            Développeur informatique passionné dans le domaine du web et mobile.
-            Je conçois et realise des sites web et des application mobiles.
-          </p>
+        
+        <h2 className={` ${IndexStyles.section__about__title} ${IndexStyles.section__titleWhite} `}>&Agrave; propos</h2>
+        <div className={IndexStyles.section__containerAbout}>
+        <div className={IndexStyles.section__about__illustration__container}>
+          <img src={aboutme}/>
+          </div>
+          <div className={IndexStyles.section__about__body}>
+            <p><strong>Développeur informatique passionné</strong> dans le domaine du web et mobile.</p> 
+            <p>Je conçois et realise des sites web et des application mobiles.</p>
+
+          </div>
+         
         </div>
       </section>
-     
+      </div>
+      <section id="contact" className={`${IndexStyles.section}             
+                                    ${IndexStyles.sectionContact}`}>
+      <h2 className={IndexStyles.section__contactform__title}>Contact</h2>
       <ContactForm id="contact"/>
+      </section>
       <Footer/>
       
   </>

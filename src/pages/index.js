2effc619078bbb4748d61f4101d0467b68e6723d
Sheron moucle <mouclesheron@gmail.com>
2020-08-26 15:38:05 -0400

    refactor:  extract Illustration  of the component to it's own image file

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,17 +6,20 @@ import SEO from "../components/seo"
 import ProjectGallery from "../components/project-gallery"
 import { Link } from "gatsby"
 import ContactForm  from "../components/contact"
-import DevIllustration from "../components/illustration/DevIllustration/index"
+import DevIllustration from "../images/dev.svg"
 import Header from "../components/header"
 import Footer from "../components/footer"
  
 import aboutme from "../images/about_me.svg"
+import useDeviceDetect from "../utils/useDeviceDetect";
 
-const IndexPage = ( ) => (
-  <>
+const IndexPage = ( ) => {
+  const { isMobile } = useDeviceDetect();
+ return( <>
   <div className={IndexStyles.wrapper}>
     <SEO title="Home" />
-       <Header/>
+       <Header isMobile={isMobile} />
+ 
       <section
         id="home"
         className={IndexStyles.sectionIntro}
@@ -25,8 +28,7 @@ const IndexPage = ( ) => (
         <h1
           className={`${IndexStyles.section__title} ${IndexStyles.section__titleIntro} ${IndexStyles.section__titleBlack}`}
         >
-            <strong>je créer les meilleur  site web et application mobile</strong>
-            <br/> Je suis Patrick shéron moucle
+            <br/> Je suis Patrick Shéron MOUCLE
              Développeur informatique
              
            
@@ -34,7 +36,7 @@ const IndexPage = ( ) => (
         <Link to="/#project" className={IndexStyles.section__intro__link}><p>Mes réalisations</p></Link>
         </div>
         <div className={IndexStyles.section__intro__illustration__container}>
-        <DevIllustration/>
+        <img   src={DevIllustration}alt="intro illustration"/>
         </div>
         
       </section>
@@ -50,7 +52,8 @@ const IndexPage = ( ) => (
         Voici des exemples de mes réalisations personnelles et professionnelles.
         </p>
          
-        <ProjectGallery className={IndexStyles.sectionProject} />
+        <ProjectGallery isMobile={isMobile}
+         className={IndexStyles.sectionProject} />
         </div>
       </section>
       <div className={IndexStyles.wrapperAbout}>
@@ -63,7 +66,7 @@ const IndexPage = ( ) => (
         <h2 className={` ${IndexStyles.section__about__title} ${IndexStyles.section__titleWhite} `}>&Agrave; propos</h2>
         <div className={IndexStyles.section__containerAbout}>
         <div className={IndexStyles.section__about__illustration__container}>
-          <img src={aboutme}/>
+          <img src={aboutme} alt="illustration about me"/>
           </div>
           <div className={IndexStyles.section__about__body}>
             <p><strong>Développeur informatique passionné</strong> dans le domaine du web et mobile.</p> 
@@ -81,6 +84,6 @@ const IndexPage = ( ) => (
       </section>
       <Footer/>
       
-  </>
-)
+  </>)
+}
 export default IndexPage

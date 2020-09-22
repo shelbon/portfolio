7826ab9f2857c7b457e85338e0ff8c0f01ecbe4f
SHERON-PC\sheron <mouclesheron@gmail.com>
2020-09-22 11:59:41 -0400

    add   cv by localization and change translation

--- src/components/UI/Nav/Nav.jsx
+++ src/components/UI/Nav/Nav.jsx
@@ -4,15 +4,22 @@ import {useStaticQuery } from "gatsby"
 import { useTranslation } from "react-i18next"
 import { FaHome, FaBriefcase, FaAddressCard,FaPhone } from "react-icons/fa"
 import { LocalizedLink } from "../../LocalizedLink"
+import { useLocalization } from "gatsby-theme-i18n"
+ 
  const  Nav = ()=> {
+   const {locale}=useLocalization();
   const { t } = useTranslation("navigation");
-  let cv = useStaticQuery(graphql`
+  let cvs = useStaticQuery(graphql`
   {
-    file(name: {regex: "/\\\\CV/"}) {
-    publicURL
+    allFile(filter:{name:{regex:"/^CV-/"}}){
+    nodes{
+      name
+      publicURL
+    }
     }
   }
 `); 
+   let cv=cvs.allFile.nodes.find(cv=>cv.name.includes(`-${locale}`));
  
     return (
       <nav
@@ -46,17 +53,17 @@ import { LocalizedLink } from "../../LocalizedLink"
               <p className={navStyles.nav__link__text}>{t('contact')}</p>
             </LocalizedLink>
           </li>
-          <li className={navStyles.nav__item}>
+          <li className={navStyles.nav__item} id={navStyles.phone}>
             <a className={navStyles.nav__link} href="tel:+596696182266">
               <FaPhone className={navStyles.nav__link__icon} />
               <p className={navStyles.nav__link__text} >{t('phone')}</p>
             </a>
           </li>
-          <li className={navStyles.nav__item}>
-            <a className={navStyles.nav__link} href={cv.file.publicURL}>
+          { cv &&(<li className={navStyles.nav__item}>
+            <a className={navStyles.nav__link}  rel="noopener noreferrer" target="_blank" href={cv.publicURL}>
               <p className={navStyles.nav__link__text}>CV</p>
             </a>
-          </li>
+          </li>)}
         </ul>
       </nav>
     )

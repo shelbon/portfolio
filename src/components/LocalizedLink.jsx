ef869c7154101de47d37f4237c7d740483040485
DESKTOP-CQRI68F\shero <mouclepatrick@protonmail.com>
2021-03-14 12:34:13 -0400

    🔵other: remove not used file

--- src/components/LocalizedLink.jsx
+++
@@ -1,9 +0,0 @@
-import { useLocalization } from "gatsby-theme-i18n"
-import { localizedPath } from "gatsby-theme-i18n/src/helpers"
-import * as React from "react"
-
-export const LocalizedLink = ({ to, language, ...props }) => {
-    const { defaultLang, locale } = useLocalization()
-    const linkLocale = language || locale
-  return <a className={props.className} href={localizedPath(defaultLang, linkLocale, to)}>{props.children}</a>
-  }

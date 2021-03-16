d89bfe3fdf0eb6755fde73e9c1ee6e5aefb0b312
DESKTOP-CQRI68F\shero <mouclepatrick@protonmail.com>
2021-03-16 18:44:54 -0400

    Revert "Update gatsby-config.js"
    This reverts commit bebd286f895020383e6c81c25b6e99d144bedd51.

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,6 +23,7 @@ module.exports = {
     "gatsby-transformer-sharp",
     "gatsby-plugin-sharp",
     "gatsby-plugin-preact",
+    "gatsby-plugin-netlify",
     "gatsby-plugin-webpack-bundle-analyser-v2",
     {
       resolve: "gatsby-source-filesystem",

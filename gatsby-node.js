4eb384670f8468ed8c327f083d04d519441c0c65
DESKTOP-CQRI68F\shero <mouclepatrick@protonmail.com>
2021-03-16 20:22:26 -0400

    🔵other: redirects with gatsby-plugin-netlify

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,6 +1,6 @@
 const fs = require("fs")
 const { graphql } = require("gatsby")
-
+const  {createRedirect}=require("gatsby-plugin-netlify")
 exports.onPreBootstrap = ({ reporter }) => {
   const contentPath = `${__dirname}/content/projects`
 
@@ -129,18 +129,13 @@ exports.onCreateNode = async (
     }
   }
 }
-
-exports.onCreatePage = async ({ page, actions }) => {
-  const { createPage, deletePage } = actions
-  // Check if the page is a localized 404
-  if (page.path.match(/^\/[a-z]{2}\/404\/$/)) {
-    const oldPage = { ...page }
-    // Get the language code from the path, and match all paths
-    // starting with this code (apart from other valid paths)
-    const langCode = page.path.split(`/`)[1]
-    page.matchPath = `/${langCode}/*`
-    // Recreate the modified page
-    deletePage(oldPage)
-    createPage(page)
-  }
+exports.createPages = ({ actions }) => {
+  const  {createRedirect}=actions
+  createRedirect({
+    fromPath: "/*",
+    toPath: "/fr/maintenance",
+    statusCode: 301,
+    force:true,
+    Language:["fr","fr-fr","fr-ca"]
+  })
 }

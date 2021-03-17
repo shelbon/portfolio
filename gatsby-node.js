fc7ddd65c5030f9de97a9813a09f700cdd308ba8
DESKTOP-CQRI68F\shero <mouclepatrick@protonmail.com>
2021-03-16 22:11:22 -0400

    ras

--- gatsby-node.js
+++ gatsby-node.js
@@ -134,7 +134,7 @@ exports.createPages = ({ actions }) => {
   createRedirect({
     fromPath: "/*",
     toPath: "/fr/maintenance",
-    statusCode: 301,
+    statusCode: 302,
     force:true,
     Language:["fr","fr-fr","fr-ca"]
   })

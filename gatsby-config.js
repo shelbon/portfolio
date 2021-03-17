1e4539b0d1045e1e0d3ca6751d8ea0de5039a425
DESKTOP-CQRI68F\shero <mouclepatrick@protonmail.com>
2021-03-17 12:41:53 -0400

    🖥️wip: attempt to fix redirects(17)

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,12 +23,7 @@ module.exports = {
     "gatsby-transformer-sharp",
     "gatsby-plugin-sharp",
     "gatsby-plugin-preact",
-    {
        resolve:"gatsby-plugin-netlify",
-      options:{
-        generateMatchPathRewrites: false
-      }
-    },
     "gatsby-plugin-webpack-bundle-analyser-v2",
     {
       resolve: "gatsby-source-filesystem",

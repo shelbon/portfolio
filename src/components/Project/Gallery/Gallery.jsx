5822426dbafd544d395f825a4ba2090f39aea4d4
shelbon <mouclepatrick@protonmail.com>
2021-04-06 09:42:17 -0400

    🔵other: remove useless line of code

--- src/components/Project/Gallery/Gallery.jsx
+++ src/components/Project/Gallery/Gallery.jsx
@@ -2,65 +2,13 @@ import React from "react"
 import { StaticQuery, graphql } from "gatsby"
 import { portfolio } from "./Gallery.module.css"
 import ProjectGalleryItem from "./Item/Item.jsx"
-import { portfolio__item__overlay, jsOverlayOpen } from "./Item/Item.module.css"
 class ProjectGallery extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      overlays: [],
-      openOverlayId: -1,
       locale: props.locale,
     }
-
-    this.overlayManager = this.overlayManager.bind(this)
-    this.showOverlay = this.showOverlay.bind(this)
-    this.closeOverlay = this.closeOverlay.bind(this)
   }
-
-  componentDidMount() {
-    this.setState(() => {
-      return {
-        overlays: [
-          ...document.getElementsByClassName(portfolio__item__overlay),
-        ],
-      }
-    })
-    this.jsOverlayOpen = jsOverlayOpen
-  }
-
-  showOverlay(id) {
-    this.setState(prevState => ({
-      overlays: prevState.overlays.map(item => {
-        if (item.dataset.overlayId === id) {
-          item.classList.add(this.jsOverlayOpen)
-          item.style.display = "flex"
-        }
-        return item
-      }),
-      openOverlayId: id,
-    }))
-  }
-  closeOverlay(id) {
-    this.setState(prevState => ({
-      overlays: prevState.overlays.map(item => {
-        if (item.dataset.overlayId === id) {
-          item.classList.remove(this.jsOverlayOpen)
-          item.style.display = "none"
-        }
-        return item
-      }),
-      openOverlayId: -1,
-    }))
-  }
-  overlayManager(id) {
-    if (this.state.openOverlayId !== -1) {
-      this.closeOverlay(this.state.openOverlayId)
-      this.showOverlay(id)
-    } else if (this.state.openOverlayId === -1) {
-      this.showOverlay(id)
-    }
-  }
-
   render() {
     return (
       <StaticQuery

87d7f6c09d59e102345d0110af910b704e0ce945
shelbon <mouclepatrick@protonmail.com>
2021-05-30 15:52:50 -0400

    🐞fix: inconsistence  of the design on  small mobile devices

--- src/components/UI/Footer/Footer.jsx
+++ src/components/UI/Footer/Footer.jsx
@@ -1,15 +1,19 @@
-import React from "react"
-import { footer, footer__details } from "./Footer.module.css"
-import { useTranslation } from "react-i18next"
+import React from 'react';
+import { footer, footer__details } from './Footer.module.css';
+import { useTranslation } from 'react-i18next';
 
-const Footer = () => {
-  const { t } = useTranslation("home")
+const Footer = ({ isMobile }) => {
+  const { t } = useTranslation('home');
+  if (isMobile) {
+    let footer = document.getElementsByTagName('footer')[0];
+    footer.style.marginBottom = 'var(--nav-height)';
+  }
   return (
-    <footer className={footer}>
+    <footer>
       <div className={footer__details}>
-        © {new Date().getFullYear()} {t("footer")}
+        © {new Date().getFullYear()} {t('footer')}
       </div>
     </footer>
-  )
-}
-export default Footer
+  );
+};
+export default Footer;

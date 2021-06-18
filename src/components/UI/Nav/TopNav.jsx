8e4d74154174805dd8ebde6df46a2a14b52834ac
shelbon <mouclepatrick@protonmail.com>
2021-06-18 15:26:01 -0400

    🌈 Style: uniformize css selectors naming

--- src/components/UI/Nav/TopNav.jsx
+++ src/components/UI/Nav/TopNav.jsx
@@ -1,43 +1,33 @@
 import { LocalizedLink } from 'gatsby-theme-i18n';
 import React from 'react';
 import { useTranslation } from 'react-i18next';
-import {
-  nav__link,
-  nav__link__text,
-  topNav
-} from './topNav.module.css';
+import { link, linkText, topNav } from './topNav.module.css';
 
 const TopNav = ({ cv, pageName }) => {
   const { t } = useTranslation('navigation');
   return (
     <nav id={topNav} aria-hidden="false">
-      <LocalizedLink className={nav__link} to="/home">
-        <p className={nav__link__text}>{t('home')}</p>
+      <LocalizedLink className={link} to="/home">
+        <p className={linkText}>{t('home')}</p>
       </LocalizedLink>
 
-      <LocalizedLink className={nav__link} to={`/${pageName}#about`}>
-        <p className={nav__link__text}>{t('about')}</p>
+      <LocalizedLink className={link} to={`/${pageName}#about`}>
+        <p className={linkText}>{t('about')}</p>
       </LocalizedLink>
-      <LocalizedLink
-        className={nav__link}
-        to={`/${pageName}#project`}
-      >
-        <p className={nav__link__text}>{t('work')}</p>
+      <LocalizedLink className={link} to={`/${pageName}#project`}>
+        <p className={linkText}>{t('work')}</p>
       </LocalizedLink>
-      <LocalizedLink
-        className={nav__link}
-        to={`/${pageName}#contact`}
-      >
-        <p className={nav__link__text}>{t('contact')}</p>
+      <LocalizedLink className={link} to={`/${pageName}#contact`}>
+        <p className={linkText}>{t('contact')}</p>
       </LocalizedLink>
       {cv && (
         <a
-          className={nav__link}
+          className={link}
           rel="noopener noreferrer"
           target="_blank"
           href={cv.publicURL}
         >
-          <p className={nav__link__text}>{t('resume')}</p>
+          <p className={linkText}>{t('resume')}</p>
         </a>
       )}
     </nav>

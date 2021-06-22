0e02223b15f91e5799bbd3ca83f9450eae40064b
shelbon <mouclepatrick@protonmail.com>
2021-06-22 10:42:48 -0400

    🔵other: update design

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,31 +1,46 @@
-import React from "react"
+import { LocalizedLink as Link } from "gatsby-theme-i18n";
+import React from "react";
+import { useTranslation } from "react-i18next";
+import SEO from "../components/seo";
+import Layout from "../templates/layout";
+import AppContext from "../utils/context";
+import {
+  errorCta,
+  errorMessage,
+  mainContent,
+  title,
+  wrapper,
+} from "./404.module.css";
 
-import SEO from "../components/seo"
-import { useTranslation } from "react-i18next"
-import { LocalizedLink as Link } from "gatsby-theme-i18n"
 const NotFoundPage = () => {
-  const { t } = useTranslation("404")
+  const { t } = useTranslation("404");
   return (
-    <>
+    <Layout>
       <SEO title="404: Not found" />
-      <div
-        style={{
-          display: "flex",
-          flexFlow: "column nowrap",
-          justifyContent: "center",
-          alignContent: "center",
-        }}
-      >
-        <div style={{ alignSelf: "center" }}>
-          <h1 style={{ color: "#ffffff" }}>{t("title")}</h1>
-          <p style={{ color: "#ffffff" }}>{t("body")} </p>
-        </div>
-        <Link to="/home" className="button">
-          <p>{t("redirect.link")}</p>
-        </Link>
-      </div>
-    </>
-  )
-}
+      <AppContext.Consumer>
+        {({ isMobile, cv }) => (
+          <div
+            className={wrapper}
+            style={{
+              marginTop: !isMobile ? "calc(var(--nav-height)*1.5)" : 0,
+            }}
+          >
+            <h1 className={title}>40</h1>
+            {/* <img
+              src={pageNotFoundIllustration}
+              alt="page not found"
+            /> */}
+            <div className={mainContent}>
+              <p className={errorMessage}>{t("title")}</p>
+            </div>
+            <Link className={` button ${errorCta}`} to="/home">
+              <p>{t("redirect.link")}</p>
+            </Link>
+          </div>
+        )}
+      </AppContext.Consumer>
+    </Layout>
+  );
+};
 
-export default NotFoundPage
+export default NotFoundPage;

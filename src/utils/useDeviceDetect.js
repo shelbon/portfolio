0414e461f1b42149df3dafb9b2da232ea08df91e
shelbon <mouclepatrick@protonmail.com>
2021-06-22 11:09:33 -0400

    🔵other: add Nokia phone to detection

--- src/utils/useDeviceDetect.js
+++ src/utils/useDeviceDetect.js
@@ -1,18 +1,18 @@
-import React from "react"
+import React from "react";
 
 export default function useDeviceDetect() {
-  const [isMobile, setMobile] = React.useState(undefined)
+  const [isMobile, setMobile] = React.useState(undefined);
 
   React.useEffect(() => {
     const userAgent =
-      typeof window.navigator === "undefined" ? "" : navigator.userAgent
+      typeof window.navigator === "undefined" ? "" : navigator.userAgent;
     const mobile = Boolean(
       userAgent.match(
-        /Android|BlackBerry|iPhone|iPad|iPod|Opera Mini|IEMobile|WPDesktop|Silk/i
+        /Android|BlackBerry|iPhone|iPad|iPod|Nokia|Opera Mini|IEMobile|WPDesktop|Silk/i
       )
-    )
-    setMobile(mobile)
-  }, [])
+    );
+    setMobile(mobile);
+  }, []);
 
-  return { isMobile }
+  return { isMobile };
 }

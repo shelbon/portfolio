f6030e448655764663470fca25dcbcff30584257
shelbon <mouclepatrick@protonmail.com>
2021-09-08 15:11:41 -0400

    ✨feature: add topbar mobile friendly

--- src/components/UI/Nav/TopNav.jsx
+++ src/components/UI/Nav/TopNav.jsx
@@ -1,35 +1,111 @@
+import bars from "@iconify/icons-la/bars";
+import times from "@iconify/icons-la/times";
+import { Icon } from "@iconify/react";
 import { LocalizedLink } from "gatsby-theme-i18n";
-import React from "react";
+import React, { useEffect } from "react";
 import { useTranslation } from "react-i18next";
-import { link, linkText, topNav } from "./topNav.module.css";
+import useToggle from "../../../hooks/useToggle";
+import {
+  hideUl,
+  link,
+  linkContainer,
+  linkText,
+  mobileMode,
+  navBarToggle,
+  navBarToggleIcon,
+  navBarToggleOpen,
+  topNav,
+  topNavUl,
+} from "./topNav.module.css";
 
 const TopNav = ({ cv, pageName }) => {
   const { t } = useTranslation("navigation");
-  return (
-    <nav id={topNav} aria-hidden="false">
-      <LocalizedLink className={link} to="/home">
-        <p className={linkText}>{t("home")}</p>
-      </LocalizedLink>
+  const [value, toggleValue] = useToggle(false);
 
-      <LocalizedLink className={link} to={`/${pageName}#about`}>
-        <p className={linkText}>{t("about")}</p>
-      </LocalizedLink>
-      <LocalizedLink className={link} to={`/${pageName}#project`}>
-        <p className={linkText}>{t("work")}</p>
-      </LocalizedLink>
-      <LocalizedLink className={link} to={`/${pageName}#contact`}>
-        <p className={linkText}>{t("contact")}</p>
-      </LocalizedLink>
-      {cv && (
-        <a
-          className={link}
-          rel="noopener noreferrer"
-          target="_blank"
-          href={cv.publicURL}
-        >
-          <p className={linkText}>{t("resume")}</p>
-        </a>
-      )}
+  return (
+    <nav id={topNav} className={value ? mobileMode : ""} aria-hidden="false">
+      <button
+        type="button"
+        className={`reset-button-style ${navBarToggle} ${
+          value ? navBarToggleOpen : ""
+        }`}
+        onClick={() => {
+          toggleValue(!value);
+        }}
+      >
+        <Icon icon={value ? times : bars} className={navBarToggleIcon} />
+      </button>
+      <ul className={`${topNavUl} ${!value ? hideUl : ""}`}>
+        <li className={linkContainer}>
+          <LocalizedLink
+            className={link}
+            to="/home"
+            onClick={() => {
+              if (value) {
+                toggleValue(!value);
+              }
+            }}
+          >
+            <p className={linkText}>{t("home")}</p>
+          </LocalizedLink>
+        </li>
+        <li className={linkContainer}>
+          <LocalizedLink
+            className={link}
+            to={`/${pageName}#about`}
+            onClick={() => {
+              if (value) {
+                toggleValue(!value);
+              }
+            }}
+          >
+            <p className={linkText}>{t("about")}</p>
+          </LocalizedLink>
+        </li>
+        <li className={linkContainer}>
+          <LocalizedLink
+            className={link}
+            to={`/${pageName}#project`}
+            onClick={() => {
+              if (value) {
+                toggleValue(!value);
+              }
+            }}
+          >
+            <p className={linkText}>{t("work")}</p>
+          </LocalizedLink>
+        </li>
+        <li className={linkContainer}>
+          <LocalizedLink
+            className={link}
+            to={`/${pageName}#contact`}
+            onClick={() => {
+              if (value) {
+                toggleValue(!value);
+              }
+            }}
+          >
+            <p className={linkText}>{t("contact")}</p>
+          </LocalizedLink>
+        </li>
+        <li className={linkContainer}>
+          {cv && (
+            <a
+              className={link}
+              rel="noopener noreferrer"
+              target="_blank"
+              href={cv.publicURL}
+              onClick={() => {
+                if (value) {
+                  toggleValue(!value);
+                }
+              }}
+            >
+              <p className={linkText}>{t("resume")}</p>
+            </a>
+          )}
+        </li>
+      </ul>
     </nav>
   );
 };

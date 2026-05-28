a3bf003a1f0f67361a9fc0ba32b1acb4d3bc9e8b
shelbon <20593460+shelbon@users.noreply.github.com>
2026-05-28 15:40:08 +0200

    perf: inline critical stylesheets

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,8 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://mouclepatrick.netlify.app',
+  build: {
+    inlineStylesheets: 'always',
+  },
   integrations: [mdx()],
 });
